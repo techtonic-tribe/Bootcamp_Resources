30c57eeb07b0c449e6f8d145df61fa489ed42226
fitsumhelina <dev.fitsum@gmail.com>
2025-05-13 22:38:05 +0300

    Add TodoList component for managing a list of items

---
+++ React/week-2/state-events/src/components/TodoList.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+function TodoList() {
+  const [items, setItems] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addItem = () => {
+    setItems(prevItems => [...prevItems, input]);
+//[...prevItems, input]: The spread operator (...) creates a new array containing all elements of prevItems, followed by the new input value at the end.
+    setInput('');
+  };
+  return (
+    <div>
+      <input value={input} onChange={e => setInput(e.target.value)} />
+      <button onClick={addItem}>Add</button>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
